8cf84b3f730a1c290d677c77468113ec224781b1
Christina Molodowtich <cmolodowitch@gmail.com>
2022-12-04 22:17:52 -0500

    Clean up cnn_train_utils.py
    Refactored to remove unused methods/code, add comments.

--- shape/cnn_train_utils.py
+++ shape/cnn_train_utils.py
@@ -9,7 +9,7 @@ from torchvision import datasets, models, transforms
 
 from timeit import default_timer as timer
 
-# batch size
+# batch sizes used for DataLoaders
 BATCH_SIZE = 32
 BATCH_SIZE_VALID = 32
 
@@ -151,14 +151,26 @@ def generate_weight_sampler(len_train: int, train_dataset) -> WeightedRandomSamp
 def generate_dataloaders_with_reserve(train_path: str, val_path: str, normalize=get_default_normalization(),
                                       resize_transform=None, weight_samples=True) -> Tuple[DataLoader, DataLoader]:
     """
-    Builds training and
+    Builds training and validation dataloaders by splitting the images in the specified train_path into separate train
+    and validation sets, and combining the split validation set with the reserved images in the specified val_path such
+    that the total ratio of training images to validation images is 80/20.
 
-    :param train_path:
-    :param val_path:
-    :param normalize:
-    :param resize_transform:
-    :param weight_samples:
-    :return:
+    That is, if the train_path has 110 images and the val_path has 15 images, the final training dataloader will have
+    100 images from the train_path and the final validation dataloader will have 10 images from the train_path plus the
+    15 reserved images from the val_path.
+
+    :param train_path: file path to a parent directory containing images sorted into subdirectories by shape, to be used
+                       for training the model
+    :param val_path: file path to a parent directory containing images sorted into subdirectories by shape, to be used
+                     for validating the trained model
+    :param normalize: normalization transform to use for both training and validation dataloaders, defaults to
+                      get_default_normalization() if not specified
+    :param resize_transform: resize transform to use for both training and validation dataloaders, defaults to None if
+                             not specified
+    :param weight_samples: True if the WeightedRandomSampler should be used for the training dataloader, with weights
+                           inversely proportional to the class distribution, False if it shouldn't be, defaults to True
+    :return: tuple containing the training and validation dataloaders built from the specified image directories and
+             options
     """
     print("TRAINING PATH: " + train_path)
     print("VALIDATION PATH: " + val_path)
@@ -236,6 +248,21 @@ def generate_dataloaders_with_reserve(train_path: str, val_path: str, normalize=
 
 def generate_dataloaders_no_reserve(train_path: str, normalize=get_default_normalization(),
                                     resize_transform=None, weight_samples=True) -> Tuple[DataLoader, DataLoader]:
+    """
+    Generates training and validation dataloaders by doing a straight 80/20 train/test split on the images in the
+    specified train_path.
+
+    :param train_path: file path to a parent directory containing images sorted into subdirectories by shape, to be used
+                       for training and testing the model
+    :param normalize: normalization transform to use for both training and validation dataloaders, defaults to
+                      get_default_normalization() if not specified
+    :param resize_transform: resize transform to use for both training and validation dataloaders, defaults to None if
+                             not specified
+    :param weight_samples: True if the WeightedRandomSampler should be used for the training dataloader, with weights
+                           inversely proportional to the class distribution, False if it shouldn't be, defaults to True
+    :return: tuple containing the training and validation dataloaders built from the specified image directory and
+             options
+    """
     print("TRAINING PATH: " + train_path)
 
     # the training transforms
@@ -300,6 +327,18 @@ def generate_dataloaders_no_reserve(train_path: str, normalize=get_default_norma
 
 def generate_train_dataloader(train_path: str, normalize=get_default_normalization(), resize_transform=None,
                               weight_samples: bool = True, shuffle: bool = False) -> DataLoader:
+    """
+    Generates the training dataloader using all the images in the specified train_path.
+
+    :param train_path: file path to a parent directory containing images sorted into subdirectories by shape, to be used
+                       for training the model
+    :param normalize: normalization transform to use for the training dataloader, defaults to
+                      get_default_normalization() if not specified
+    :param resize_transform: resize transform to use for the training dataloader, defaults to None if not specified
+    :param weight_samples: True if the WeightedRandomSampler should be used for the dataloader, with weights inversely
+                           proportional to the class distribution, False if it shouldn't be, defaults to True
+    :return: training dataloader built from the specified image directory and options
+    """
     print("TRAINING PATH: " + train_path)
 
     # the training transforms
@@ -341,20 +380,16 @@ def generate_train_dataloader(train_path: str, normalize=get_default_normalizati
     return train_data_loader
 
 
-def define_densenet_model(num_classes: int, n_unfrozen: int = None):
-    torch.cuda.empty_cache()
-    model = models.densenet121(weights=models.DenseNet121_Weights.DEFAULT)
-    if n_unfrozen is not None:
-        use_last_n_layers(n_unfrozen)
-    num_ftrs = model.classifier.in_features
-    model.classifier = nn.Linear(num_ftrs, num_classes)
-    model.train()
-    model.cuda()
-    print(f"DENSENET, {'ALL' if n_unfrozen is None else n_unfrozen} LAYERS UNFROZEN")
-    return model
-
-
 def use_last_n_layers(model, n_unfrozen_layers: int) -> int:
+    """
+    Freezes all but the last n_unfrozen_layers on the specified model.
+
+    If the model has fewer layers than n_unfrozen_layers, leaves all layers unfrozen.
+
+    :param model: PyTorch CNN model for which the last n_unfrozen_layers should be left active for finetuning.
+    :param n_unfrozen_layers: number of layers to leave unfrozen
+    :return: total number of layers in the model
+    """
     # First count the layers (model.parameters() doesn't have a len implementation, so do this manually)
     n_layers = 0
     for _ in model.parameters():
@@ -392,6 +427,16 @@ def freeze_layers(model, n_unfrozen_layers: int) -> int:
 
 # https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tutorial.html
 def initialize_model(model_name: str, num_classes: int, n_unfrozen: int = None):
+    """
+    Load the model with the specified name and initializes it using default pre-trained weights, modifying it to have
+    the final layer output the specified num_classes, optionally specifying the number of layers to leave unfrozen.
+
+    :param model_name: name of the model to load and initialize with PyTorch default pre-trained weights for that model
+    :param num_classes: number of classes that the final model layer should output
+    :param n_unfrozen: number of layers to leave unfrozen, specify 0 to freeze all layers or None to leave all layers
+                       unfrozen, defaults to None
+    :return: tuple containing the initialized model and the default image size to use for model input
+    """
     # Initialize these variables which will be set in this if statement. Each of these
     #   variables is model specific.
     model_ft = None
@@ -485,13 +530,32 @@ def initialize_model(model_name: str, num_classes: int, n_unfrozen: int = None):
 
 
 def generate_timestamp() -> str:
+    """
+    Generates a timestamp string of the current date and time, for use in labeling saved model files.
+
+    :return: timestamp string of the current date and time, for use in labeling saved model files
+    """
     now = datetime.now()
     return now.strftime("%Y%m%d_%H%M%S")
 
 
 def save_model(epochs, model, optimizer, criterion, model_path: str, val_acc: float = None) -> None:
     """
-    Function to save the trained model to disk.
+    Saves the trained model to disk, including epochs, model state_dict, optimizer state_dict, and criterion.
+
+    Saved object structure:
+    {
+        'epoch': epochs,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'loss': criterion,
+    }
+
+    Builds the name of the model file from the specified model_path as a prefix, adding a timestamp and the validation
+    accuracy to the model file name.
+
+    For later use in validation or generating inferences, the model_state_dict should be loaded into an initialized
+    model of the same type.
     """
     extension = generate_timestamp()
     extension = extension + "_" + str(val_acc) + "_"
@@ -512,17 +576,27 @@ def load_model(model, model_path: str):
     return model
 
 
-def validate_it(model, test_loader, get_outputs=lambda outputs: outputs) -> float:
+def validate_it(model, test_loader: DataLoader, get_outputs=lambda outputs: outputs) -> float:
+    """
+    Runs all images in the specified test_loader DataLoader against the specified model, tabulating the overall
+    prediction accuracy of the model predictions against the actual labels, in range [0, 1].
+
+    :param model: PyTorch model against which the images in the specified test_loader should be run
+    :param test_loader: PyTorch dataloader containing images to test for accuracy against the specified model
+    :param get_outputs: function that extracts the actual model outputs from the object generated by calling the
+                        model on a set of images from the data loader, present in case the model output structure
+                        contains multiple parts (as for the Inception v3 CNN), defaults to simply returning the
+                        outputs object
+    :return: accuracy for predictions by the model across all images in the specified test_loader, in range [0, 1]
+    """
     ### TESTING PORTION ###
     start = timer()
     print("validating model")
     correct = 0
     total = 0
-    # since we're not training, we don't need to calculate the gradients for our outputs
     with torch.no_grad():
         for data in test_loader:
             images, labels = data
-            # print(labels)
             # calculate outputs by running images through the network
             images = images.cuda()
             labels = labels.cuda()
@@ -540,24 +614,39 @@ def validate_it(model, test_loader, get_outputs=lambda outputs: outputs) -> floa
     return acc
 
 
-def calculate_loss(criterion, outputs, labels):
-    loss = criterion(outputs, labels)
-    return loss
+def pytorch_train_and_evaluate(model, train_loader: DataLoader, test_loader: DataLoader,
+                               model_path_prefix: str, epoch_num=10,
+                               get_outputs=lambda outputs: outputs,
+                               calc_loss=lambda criterion, outputs, labels: criterion(outputs, labels)) -> None:
+    """
+    Fine-tunes the specified PyTorch model using the images in the specified train_loader, validating after each epoch
+    using the images in the specified test_loader.  Runs for the specified number of epochs.  After each epoch, saves
+    the current model state using the specified model path prefix.
 
+    For Densenet, the get_outputs and calc_loss lambda arguments can be left as default - these were added to allow use
+    with Inception v3, which has outputs in a different format, and has two separate optimizers whose loss should be
+    combined.
 
-def pytorch_train_and_evaluate(model, train_loader, test_loader, model_path: str, epoch_num=5,
-                               get_outputs=lambda outputs: outputs, calc_loss=calculate_loss):
+    :param model: PyTorch model to fine tune and validate using the specified images
+    :param train_loader: PyTorch DataLoader containing images to be used in training the model
+    :param test_loader: PyTorch DataLoader containing images to be using in validating the model
+    :param model_path_prefix: prefix for the saved model file
+    :param epoch_num: number of epochs to fine tune, defaults to 10
+    :param get_outputs: lambda used to extract the actual predictions from the model outputs, defaults to just returning
+                        the outputs object
+    :param calc_loss: lambda used to calculate the loss for the data run through the training, defaults to the value
+                      returned by running the criterion with the outputs and labels
+    """
     ### TRAINING PORTION ###
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
     print("perform initial validation check: ")
     val_acc = validate_it(model, test_loader, get_outputs)
     print("perform model save check: ")
-    save_model(0, model, optimizer, criterion, model_path, val_acc=val_acc)
+    save_model(0, model, optimizer, criterion, model_path_prefix, val_acc=val_acc)
     for epoch in range(epoch_num):  # loop over the dataset multiple times
         print("TRAINING EPOCH: " + str(epoch))
         running_loss = 0.0
-        images_processed = 0
         start = timer()
         for i, data in enumerate(train_loader, 0):
             # get the inputs; data is a list of [inputs, labels]
@@ -567,8 +656,6 @@ def pytorch_train_and_evaluate(model, train_loader, test_loader, model_path: str
 
             # zero the parameter gradients
             optimizer.zero_grad()
-            # print(inputs.shape)
-            # forward + backward + optimize
             outputs = model(inputs)
             loss = calc_loss(criterion, outputs, labels)
             loss.backward()
@@ -580,7 +667,7 @@ def pytorch_train_and_evaluate(model, train_loader, test_loader, model_path: str
                 print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 100:.3f}')
                 running_loss = 0.0
         val_acc = validate_it(model, test_loader, get_outputs)  # *validate after each epoch
-        save_model(epoch, model, optimizer, criterion, model_path, val_acc=val_acc)
+        save_model(epoch, model, optimizer, criterion, model_path_prefix, val_acc=val_acc)
         end = timer()
         print("EPOCH TIME")
         print("Seconds: " + str(end - start))  # Time in seconds, e.g. 5.38091952400282
